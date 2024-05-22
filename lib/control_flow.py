0a0690d2b299b7a8741211a0df4f2fd727b40157
kevin mayaka <kevinabuga3@gmail.com>
2024-05-22 08:09:01 +0300

    initial commit

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,48 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == 12345:
+        return "Access Granted"
+    else:
+        return "Access Denied"
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num%5== 0 and num%3 == 0:
+        return "FizzBuzz"
+    elif num%3 == 0:
+        return "Fizz"
+    elif num %5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == '+':
+        return num1 + num2
+    elif operation == '-':
+        return num1 - num2
+    elif operation == '*':
+        return num1 * num2
+    elif operation == '/':
+        # Check to avoid division by zero
+        if num2 == 0:
+            return "Cannot divide by zero"
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+        return None
+
+    
